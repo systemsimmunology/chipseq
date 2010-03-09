d1e2eefd0c4189036b2285b742778eb3c482ee8f
thorsson <thorsson@5021c4a6-a05a-4b22-8634-7b27b77db61f>
2010-03-09 22:23:10 +0000

    git-svn-id: http://svn.systemsbiology.net/repos/compbio/Vesteinn_scripts/chipseq/trunk@9651 5021c4a6-a05a-4b22-8634-7b27b77db61f

--- dev/olap.R
+++ dev/olap.R
@@ -15,21 +15,55 @@ bsub <- function(instring){substring(instring,15,nchar(instring)-4)}
 bsimp <- as.character(sapply(bfiles,bsub))
 bsimp[5] <- "C_BMM_LPS_0120_P65" ## 1349A is the db entry ?                
 
+rownames(olap) <- bsimp
+colnames(olap) <- bsimp
+
+rownames(odiff) <- bsimp
+colnames(odiff) <- bsimp
+
 fracolap <- olap / ( olap + odiff + t(odiff))
 rownames(fracolap) <- bsimp
 colnames(fracolap) <- bsimp
-
 
 image(fracolap)
 heatmap(fracolap, Rowv=NA, Colv=NA, margins=c(15,15),revC=TRUE,scale="none", col = brewer.pal(9,"Blues"))
 
 #biocLite("Heatplus")
-heatmap_2(fracolap,do.dendro=c(FALSE,FALSE), legend = 2, scale="none")
- 
-fracdiff <- 1 - odiff / ( olap + odiff )  
+heatmap_2(fracolap,do.dendro=c(FALSE,FALSE), legend = 2, scale="none", legfrac=7, col = brewer.pal(9,"Blues"))
+
+revcols <- function (mat){
+  nc <- ncol(mat)
+  mat[,seq(nc,1,-1)]
+}
+revrows <- function (mat){
+  nc <- ncol(mat)
+  mat[seq(nc,1,-1),]
+}
+
+## How much of signal in 1 is not in 2
+fracdiff <- odiff / ( olap + odiff )  
 rownames(fracdiff) <- bsimp
 colnames(fracdiff) <- bsimp
 
-image(fracdiff)
-heatmap(fracdiff, Rowv=NA, Colv=NA, margins=c(15,15),revC=TRUE,scale="none", col = brewer.pal(9,"Blues"))
+png=TRUE
+
+if (!png) {x11()} else {png("Overlap_bpairs.png", height=700, width=700)}
+main="Base pair overlap"
+heatmap3(revrows(olap),do.dendro=c(FALSE,FALSE), main=main,legend = 2, scale="none", legfrac=7, col = brewer.pal(9,"Blues"),Rowv=NA,Colv=NA)
+if (png) {dev.off()}
+
+if (!png) {x11()} else {png("Overlap_frac.png", height=700, width=700)}
+main="Fractional overlap"
+heatmap3(revrows(fracolap),do.dendro=c(FALSE,FALSE),main=main,legend = 2, scale="none", legfrac=7, col = brewer.pal(9,"Blues"),Rowv=NA,Colv=NA)
+if (png) {dev.off()}
+
+if (!png) {x11()} else {png("Diff_bpairs.png", height=700, width=700)}
+main="Region in row but not column (Base pairs) "
+heatmap3(revrows(odiff),do.dendro=c(FALSE,FALSE), main=main,legend = 2, scale="none", legfrac=7, col = brewer.pal(9,"Blues"),Rowv=NA,Colv=NA)
+if (png) {dev.off()}
+
+if (!png) {x11()} else {png("Diff_frac.png", height=700, width=700)}
+main="Region in row but not column (Fractional)"
+heatmap3(revrows(fracdiff),do.dendro=c(FALSE,FALSE), main=main,legend = 2, scale="none", legfrac=7, col = brewer.pal(9,"Blues"),Rowv=NA,Colv=NA)
+if (png) {dev.off()}
 
