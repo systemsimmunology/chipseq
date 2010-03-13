3306e850ffb502aeb9156b786972a3d6e6ff4616
thorsson <thorsson@5021c4a6-a05a-4b22-8634-7b27b77db61f>
2010-03-13 16:37:30 +0000

    git-svn-id: http://svn.systemsbiology.net/repos/compbio/Vesteinn_scripts/chipseq/trunk@9686 5021c4a6-a05a-4b22-8634-7b27b77db61f

--- utils/multi_compare.sh
+++ utils/multi_compare.sh
@@ -5,40 +5,46 @@
 #
 
 WRONGARGS=1
-if [ $# != 3 ]
+if [ $# != 4 ]
 then
-  echo "Usage: `basename $0` <bedfile list> <bedfile folder> <overlap code 1, 2, or 3>" >&2
+  echo "Usage: `basename $0` <bedfile list A> <bedfile list B> <bedfile folder> <overlap code 1, 2, or 3>" >&2
   exit $WRONGARGS
 fi
 
-bfilelist=$1
-bed_dir=$2
-code=$3
+bfilelistA=$1
+bfilelistB=$2
+bed_dir=$3
+code=$4
 
-bfiles=( $(cat $bfilelist ) )
-nb=${#bfiles[@]}
+bfilesA=( $(cat $bfilelistA ) )
+nbA=${#bfilesA[@]}
+
+bfilesB=( $(cat $bfilelistB ) )
+nbB=${#bfilesB[@]}
 
 DIR="$HOME/chipseq/utils"
 
+## A will be rows and B columns
+
 ## print header ( echo -ne is no line and allow special characters, respectively )
 echo -ne '\t' ## leading tab for column
-for (( i=0 ; i<(nb-1) ; i++ )); do
-    bfile=${bfiles[$i]}
+for (( i=0 ; i<(nbB-1) ; i++ )); do
+    bfile=${bfilesB[$i]}
     echo -ne $bfile'\t'
 done
-bfile=${bfiles[(nb-1)]} ## last entry , no tab
+bfile=${bfilesB[(nbB-1)]} ## last entry , no tab
 echo -ne $bfile
 echo -ne '\n' ## endline
 
-for (( i=0 ; i<nb ; i++ )); do
-    bfile_i=${bfiles[$i]}
+for (( i=0 ; i<nbA ; i++ )); do
+    bfile_i=${bfilesA[$i]}
     echo -ne $bfile_i'\t'
-    for (( j=0 ; j<(nb-1) ; j++ )); do
-	bfile_j=${bfiles[$j]}
+    for (( j=0 ; j<(nbB-1) ; j++ )); do
+	bfile_j=${bfilesB[$j]}
 	c=($($DIR/pybed_all_chromo.sh $bed_dir$bfile_i $bed_dir$bfile_j $code))
 	echo -ne $c'\t' ## -n no line -e tab
     done
-    bfile_j=$bed_dir${bfiles[(nb-1)]}
+    bfile_j=$bed_dir${bfilesB[(nbB-1)]}
     c=($($DIR/pybed_all_chromo.sh $bed_dir$bfile_i $bed_dir$bfile_j $code))
     echo $c ## newline, no tab for last
 done
