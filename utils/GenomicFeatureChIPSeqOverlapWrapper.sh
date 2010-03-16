22c663b4cb57b8f51810d3a06e0fd9c3c437603b
thorsson <thorsson@5021c4a6-a05a-4b22-8634-7b27b77db61f>
2010-03-16 05:19:26 +0000

    git-svn-id: http://svn.systemsbiology.net/repos/compbio/Vesteinn_scripts/chipseq/trunk@9705 5021c4a6-a05a-4b22-8634-7b27b77db61f

--- utils/GenomicFeatureChIPSeqOverlapWrapper.sh
+++ utils/GenomicFeatureChIPSeqOverlapWrapper.sh
@@ -25,7 +25,21 @@ ofile=$bedID.olap.tsv
 
 echo "Creating:" $ofile
 ../utils/GenomicFeatureChIPSeqOverlap.py temp.bed ../annotation/GenomeAnnotationsM37.bed $bedID > $ofile
-##rm -f temp.bed
+rm -f temp.bed
 
+awk '{print $1}' $ofile > ensids 
+~/bin/reMap.py ensids  1 ../annotation/mart_export.txt 2 3 > ensids.eids
+~/bin/reMap.py ensids.eids  1 ~/data/ncbi/gene_info_simplified_mouse 1 2 > ensids.gname
 
+awk '{OFS="\t"; print $2,$3,$4,$5,$6,$7,$8}' $ofile > t1
+paste ensids ensids.eids ensids.gname t1 > t2
+rm -f ensids ensids.eids ensids.gname
+
+newheader="Genome Feature\tEnsemble ID\tSymbol\tFractional Overlap\tLength of Overlap\tLength of Genome Feature\tFeature Chromosome\tFeature Start\tFeature End\tFeature Strand"
+
+tail +2 t2 > t3
+echo -e $newheader > t4
+finalfile=$bedID-geneoverlap.tsv
+cat t4 t3 > $finalfile
+rm -f t1 t2 t3 t4
 
