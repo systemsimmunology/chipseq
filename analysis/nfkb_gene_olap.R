59c20e56c2e3856a11d688160204971017efc47e
thorsson <thorsson@5021c4a6-a05a-4b22-8634-7b27b77db61f>
2010-03-23 00:49:07 +0000

    git-svn-id: http://svn.systemsbiology.net/repos/compbio/Vesteinn_scripts/chipseq/trunk@9742 5021c4a6-a05a-4b22-8634-7b27b77db61f

---
+++ analysis/nfkb_gene_olap.R
@@ -0,0 +1,13 @@
+
+olap.gene <- as.vector(as.matrix(read.table("gene_nfkb_overlap.tsv",sep="\t", header=TRUE, as.is=TRUE,row.names=1)))
+gene.minus.nfkb <- as.vector(as.matrix(read.table("gene_minus_nfkb.tsv",sep="\t", header=TRUE, as.is=TRUE,row.names=1)))
+nfkb.minus.gene <- as.vector(as.matrix(read.table("nfkb_minus_gene.tsv",sep="\t", header=TRUE, as.is=TRUE,row.names=1)))
+
+names(olap.gene) <- bsimp
+names(gene.minus.nfkb) <- bsimp
+names(nfkb.minus.gene) <- bsimp
+
+fg <- olap.gene/(gene.minus.nfkb+olap.gene+nfkb.minus.gene)
+
+
+
---
+++ analysis/polIIgeneAnalyze.R
@@ -0,0 +1,56 @@
+
+load("/Users/thorsson/allarrays/dev/lps.RData")
+
+rt <- read.table("/Users/thorsson/chipseq/dev/20090529_1922_A_BMM_LPS_0240_PolII-geneoverlap.tsv",sep="\t",as.is=TRUE,header=TRUE)
+
+rt[["Entrez.ID"]] <- rt[["Ensemble.ID"]] ## tempfix
+
+inboth <- intersect(row.names(dm.lps.exon),rt[["Entrez.ID"]])
+
+inds <- which(rt[["Entrez.ID"]] %in% inboth )
+
+fracolap <- rt[["Fractional.Overlap"]][inds]
+names(fracolap) <- rt[["Entrez.ID"]][inds]
+
+glen <- rt[["Length.of.Genome.Feature"]][inds]
+names(glen) <- rt[["Entrez.ID"]][inds]
+
+plot(fracolap,dm.lps.exon[names(fracolap),"BMDM_Bl6_LPS_0240___Female"])
+
+
+plot(fracolap,log(dm.lps.exon[names(fracolap),"BMDM_Bl6_LPS_0360___Female"]))
+
+x11()
+
+xlab <- "PolII Fractional Transcript Overlap"
+ylab <- "Transcript Length"
+plot(rt[["Fractional.Overlap"]],rt[["Length.of.Genome.Feature"]])
+
+fs <- fracolap[which(glen<20000)]
+
+plot(fs,log(dm.lps.exon[names(fs),"BMDM_Bl6_LPS_0240___Female"]))
+
+x11()
+par(mfrow=c(3,2))
+xlab <- "log2(expression)"
+
+fs.1 <- which(fs<0.2)
+main<-"fracolap < 0.2"
+hist(log2(dm.lps.exon[names(fs.1),"BMDM_Bl6_LPS_0240___Female"]),main=main,xlab=xlab)
+
+fs.2 <- which(fs >= 0.2 & fs<0.4)
+main<-"0.2 < fracolap < 0.4"
+hist(log2(dm.lps.exon[names(fs.2),"BMDM_Bl6_LPS_0240___Female"]),main=main,xlab=xlab)
+
+fs.3 <- which(fs >= 0.4 & fs<0.6)
+main<-"0.4 < fracolap < 0.6"
+hist(log2(dm.lps.exon[names(fs.3),"BMDM_Bl6_LPS_0240___Female"]),main=main,xlab=xlab)
+
+fs.4 <- which(fs >= 0.6 & fs<0.8)
+main<-"0.6 < fracolap < 0.8"
+hist(log2(dm.lps.exon[names(fs.4),"BMDM_Bl6_LPS_0240___Female"]),main=main,xlab=xlab)
+
+main<-"fracolap > 0.8"
+fs.5 <- which(fs>0.8)
+hist(log2(dm.lps.exon[names(fs.5),"BMDM_Bl6_LPS_0240___Female"]),main=main,xlab=xlab)
+
