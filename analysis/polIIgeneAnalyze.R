5d7d92b5ebae2cad392e8709da095a9941e7eeaf
thorsson <thorsson@5021c4a6-a05a-4b22-8634-7b27b77db61f>
2010-04-15 23:20:14 +0000

    git-svn-id: http://svn.systemsbiology.net/repos/compbio/Vesteinn_scripts/chipseq/trunk@9810 5021c4a6-a05a-4b22-8634-7b27b77db61f

--- analysis/polIIgeneAnalyze.R
+++ analysis/polIIgeneAnalyze.R
@@ -4,6 +4,7 @@ source("/Users/thorsson/allarrays/utils/utilitiesPlot.R")
 load("/Users/thorsson/data/ncbi/gene.symbol.RData")
 load("/Users/thorsson/data/ncbi/gene.eid.RData")
 
+
 ##load("../data/lps.RData")
 
 load("~/allarrays/data/20100407.curated.exon/CSSs.tc.RData")
@@ -32,10 +33,10 @@ plot(fracolap,dm.lps.exon[names(fracolap),"BMDM_Bl6_LPS_0240___Female"])
 plot(fracolap,log(dm.lps.exon[names(fracolap),"BMDM_Bl6_LPS_0360___Female"]))
 
 x11()
-png(file="LPS4hrsOlapVsTLength.png", width=800,height=600)
+png(file="/Volumes/thorsson/Posters/ISBSymposium2010/EpiTransPoster/LPS4hrsOlapVsTLength.png", width=800,height=600)
 
-main <- "PolII ChIPSeq 4hr LPS (SLIMseq 1922)"
-xlab <- "PolII Fractional Transcript Overlap"
+main <- "PolII ChIPSeq 4hr LPS"
+xlab <- "PolII Fractional Coverage"
 ylab <- "Transcript Length"
 plot(rt[["Fractional.Overlap"]],rt[["Length.of.Genome.Feature"]],xlab=xlab,ylab=ylab,main=main)
 
@@ -46,28 +47,28 @@ fs <- fracolap[which(glen<20000)]
 plot(fs,log(dm.lps.exon[names(fs),"BMDM_Bl6_LPS_0240___Female"]))
 
 x11()
-png(file="LPS4hrsOlapVsExp.png", height=800,width=600)
+png(file="/Volumes/thorsson/Posters/ISBSymposium2010/EpiTransPoster/LPS4hrsOlapVsExp.png", height=800,width=600)
 
 par(mfrow=c(3,2))
 xlab <- "log2(expression)"
 
 fs.1 <- which(fs<0.2)
-main<-"fracolap < 0.2"
+main<-"Fractional Coverage < 0.2"
 hist(log2(dm.lps.exon[names(fs.1),"BMDM_Bl6_LPS_0240___Female"]),main=main,xlab=xlab)
 
 fs.2 <- which(fs >= 0.2 & fs<0.4)
-main<-"0.2 < fracolap < 0.4"
+main<-"0.2 < Fractional Coverage < 0.4"
 hist(log2(dm.lps.exon[names(fs.2),"BMDM_Bl6_LPS_0240___Female"]),main=main,xlab=xlab)
 
 fs.3 <- which(fs >= 0.4 & fs<0.6)
-main<-"0.4 < fracolap < 0.6"
+main<-"0.4 < Fractional Coverage < 0.6"
 hist(log2(dm.lps.exon[names(fs.3),"BMDM_Bl6_LPS_0240___Female"]),main=main,xlab=xlab)
 
 fs.4 <- which(fs >= 0.6 & fs<0.8)
-main<-"0.6 < fracolap < 0.8"
+main<-"0.6 < Fractional Coverage < 0.8"
 hist(log2(dm.lps.exon[names(fs.4),"BMDM_Bl6_LPS_0240___Female"]),main=main,xlab=xlab)
 
-main<-"fracolap > 0.8"
+main<-"Fractional Coverage > 0.8"
 fs.5 <- which(fs>0.8)
 hist(log2(dm.lps.exon[names(fs.5),"BMDM_Bl6_LPS_0240___Female"]),main=main,xlab=xlab)
 
@@ -196,6 +197,7 @@ for ( eid in u.eids ){
 rownames(frac.olap) <- u.eids
 
 
+### Primary and Secondary response genes
 ### Need to separate into the subgroups
 rt <- as.character(read.table("/Users/thorsson/chipseq/annotation/Ramirez_Carozzi_gene_list_eid.txt")$V1)
 primary.response.eids <- rt[1:26]
@@ -215,14 +217,19 @@ a <- apply(dm.lps.3prime[,cs],1,max)
 rats <- dm.lps.3prime[,cs[2:8]]/dm.lps.3prime[,1]
 b <- log(apply(rats,1,max))
 
+## Keep only genes exceeding an absolute and log ratio treshold
 sete <- names(which(a>=300 & abs(b)>=log(3.)))
+
+## Make sure we have binding measurements
 setf <- intersect(row.names(frac.olap),sete)
 
 ## frac.olap is between 0 and 1
 ## how do we define "interesting"
 
 c <- apply(frac.olap,1,max)
+## Keep only profiles with fractional overlap above a certain threshold
 setg <- names(which(c>0.2))
+
 
 seth <- intersect(setg,setf)
   
@@ -236,7 +243,26 @@ kinplot <- function (eid) {
   plotCSS(eid,CSSs.tc.3prime[["BMDM_Bl6_LPS__Female"]],data.matrix=dm.lps.3prime,tmax=8)
 }
 
-combo <- cbind(frac.olap[seth,],dm.lps.3prime[seth,1:8])
+##combo <- cbind(frac.olap[seth,],dm.lps.3prime[seth,1:8])
+
+library(RColorBrewer)
+source("/Users/thorsson/chipseq/utils/heatmap3.R")
+
+x11()
+
+## Conditions to simplify figure
+csconds <- c("t=0","t=1hr (rep1)","t=1hr (rep2)","t=2hr","t=4hr (rep1)","t=4hr (rep2)","t=6hr")
+colnames(frac.olap) <- csconds
+
+heatmap3(frac.olap[seth,],do.dendro=c(TRUE,FALSE), main="",legend = 2, scale="none", legfrac=7, col = brewer.pal(9,"Blues"),Colv=NA,rowlab=FALSE)
+
+heatmap(frac.olap[seth,],Colv=NA, margins=c(15,15),revC=TRUE,scale="none", col = brewer.pal(9,"Blues"))
+
+heatmap(frac.olap[seth,],Colv=NA, margins=c(15,15),revC=TRUE,scale="none", col = brewer.pal(9,"Blues"),labRow=FALSE)
+
+png(file="/Volumes/thorsson/Posters/ISBSymposium2010/EpiTransPoster/heatmap1.png")
+
+png(file="/Volumes/thorsson/Posters/ISBSymposium2010/EpiTransPoster/heatmap3.png")
 
 hc <- hclust(dist(frac.olap[seth,]))                                
 all.cluster.members <- cutree(hc,4)
@@ -266,8 +292,6 @@ profileplot(dm.lps.3prime[c2,],main="",ylim=c(0,10000))
 profileplot(dm.lps.3prime[c3,],main="",ylim=c(0,10000))
 profileplot(dm.lps.3prime[c4,],main="",ylim=c(0,10000))
 
-
-
 par(mfrow=c(4,2))
 profileplot(frac.olap[c1,],main="",ylim=c(0,1))
 profileplot(dm.lps.3prime[c1,],main="",ylim=c(0,10000))
@@ -278,4 +302,55 @@ profileplot(dm.lps.3prime[c3,],main="",ylim=c(0,10000))
 profileplot(frac.olap[c4,],main="",ylim=c(0,1))
 profileplot(dm.lps.3prime[c4,],main="",ylim=c(0,10000))
 
+x11()
+par(mfrow=c(4,2))
+plot(apply(frac.olap[c1,],2,median),type='l')
+plot(apply(dm.lps.3prime[c1,],2,median),type='l')
+plot(apply(frac.olap[c2,],2,median),type='l')
+plot(apply(dm.lps.3prime[c2,],2,median),type='l')
+plot(apply(frac.olap[c3,],2,median),type='l')
+plot(apply(dm.lps.3prime[c3,],2,median),type='l')
+plot(apply(frac.olap[c4,],2,median),type='l')
+plot(apply(dm.lps.3prime[c4,],2,median),type='l')
 
+
+##
+simpcolheads <- c("0","20 min","40 min","1 hr","80 min","2 hr","4 hr","6 hr","8 hr","12 hr","18 hr", "24 hr")
+colnames(dm.lps.3prime) <- simpcolheads
+
+goldratio <- (1+sqrt(5))/2
+
+png(file="/Volumes/thorsson/Posters/ISBSymposium2010/EpiTransPoster/EarlyPolII.png",height=480,width=(goldratio*480))
+profileplot(frac.olap[c4,],main="",ylim=c(0,1))
+dev.off()
+
+png(file="/Volumes/thorsson/Posters/ISBSymposium2010/EpiTransPoster/EarlyPolIIExpression.png",height=480,width=(goldratio*480))
+profileplot(dm.lps.3prime[c4,1:8],main="",ylim=c(0,12000))
+dev.off()
+
+
+png(file="/Volumes/thorsson/Posters/ISBSymposium2010/EpiTransPoster/LatePolII.png",height=480,width=(goldratio*480))
+profileplot(frac.olap[c2,],main="",ylim=c(0,1))
+dev.off()
+
+png(file="/Volumes/thorsson/Posters/ISBSymposium2010/EpiTransPoster/LatePolIIExpression.png",height=480,width=(goldratio*480))
+profileplot(dm.lps.3prime[c2,1:8],main="",ylim=c(0,12000))
+dev.off()
+
+png(file="/Volumes/thorsson/Posters/ISBSymposium2010/EpiTransPoster/SustainedPolII.png",height=480,width=(goldratio*480))
+profileplot(frac.olap[c3,],main="",ylim=c(0,1))
+dev.off()
+
+png(file="/Volumes/thorsson/Posters/ISBSymposium2010/EpiTransPoster/SustainedPolIIExpression.png",height=480,width=(goldratio*480))
+profileplot(dm.lps.3prime[c3,1:8],main="",ylim=c(0,12000))
+dev.off()
+
+
+
+##
+## Could also look at correlation distance
+##
+
+cordist <- 1-cor(t(frac.olap[seth,]))
+
+## can't seem to get into heatmap right away
