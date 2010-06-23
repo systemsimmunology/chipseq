c6bc9f98041340835c7a062fbbf94fcdc5f36110
thorsson <thorsson@5021c4a6-a05a-4b22-8634-7b27b77db61f>
2010-06-23 23:03:39 +0000

    git-svn-id: http://svn.systemsbiology.net/repos/compbio/Vesteinn_scripts/chipseq/trunk@9867 5021c4a6-a05a-4b22-8634-7b27b77db61f

--- analysis/PoisedGenes.R
+++ analysis/PoisedGenes.R
@@ -3,3 +3,24 @@
 #awk '{print $8}' poised-genes.tab | awk -F "/" '{print $1}' > poised_eid 
 
 poised.eid <-as.character(read.table("/Users/thorsson/chipseq/analysis/poised_eid")$V1)
+
+poised.eid.expressed <-as.character(read.table("/Users/thorsson/chipseq/analysis/expr_vs_poised.tab")$V1)[-1]
+poised.eid.expressed <- poised.eid[which(poised.eid.expressed=="T")]
+
+m1 <- as.character(read.table(file.path(Sys.getenv("DATA_DIR"),"MacPolarization/ClassicalM1.tsv"),as.is=TRUE,sep='\t',header=TRUE)$Gene.ID)
+
+m2a <- as.character(read.table(file.path(Sys.getenv("DATA_DIR"),"MacPolarization/WoundHealingM2a.tsv"),as.is=TRUE,sep='\t',header=TRUE)$Gene.ID)
+
+m2b <- as.character(read.table(file.path(Sys.getenv("DATA_DIR"),"MacPolarization/RegulatoryMacsM2b.tsv"),as.is=TRUE,sep='\t',header=TRUE)$Gene.ID)
+
+cat(length(m1)," annotated Classicallly activated mac (M1) genes", "\n")
+cat(length(m2a)," annotated wound-healing mac (M2a) genes", "\n")
+cat(length(m2b)," annotated regulatory mac (M2b)", "\n")
+
+gene.symbol[intersect(m1,poised.eid)]
+gene.symbol[intersect(m2a,poised.eid)]
+gene.symbol[intersect(m2b,poised.eid)]
+
+intersect(m1,poised.eid) %in% poised.eid.expressed
+intersect(m2a,poised.eid) %in% poised.eid.expressed
+intersect(m2b,poised.eid) %in% poised.eid.expressed
