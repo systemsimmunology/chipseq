a4940a83a8bcc96f1fa5884248520046e91babc5
thorsson <thorsson@5021c4a6-a05a-4b22-8634-7b27b77db61f>
2012-08-01 04:15:17 +0000

    sigint by gene area
    git-svn-id: http://svn.systemsbiology.net/repos/compbio/Vesteinn_scripts/chipseq/trunk@10230 5021c4a6-a05a-4b22-8634-7b27b77db61f

--- analysis/PolIISigintClusters.R
+++ analysis/PolIISigintClusters.R
@@ -15,15 +15,22 @@ meansig <- apply(polIIgene.nm.sigint[,c(1,2,4,5)],1,mean,na.rm=T)
 sdsig <- apply(polIIgene.nm.sigint[,c(1,2,4,5)],1,sd,na.rm=T)
 cvsig <- sdsig/meansig
 
+##Earlier scheme
+if (FALSE) {
 keepers <- names(which(maxsig>3.779092))
 #2.267455  2.645365  3.023274  3.401183  3.779092  4.157001  ...
 #peak scores turn out to be multiples of a value around 0.3779092.
 #Thus, the values above correspond to counts
 #6, 7, 8, 9, 10, 11 ...
-
 highervars <- names(which(cvsig[keepers]>=0.5))
 data.mat <- polIIgene.nm.sigint[highervars,c(1,2,4,5)]
+}
 
+set1 <- names(which(maxsig>0.5))
+set2 <- names(which( (cvsig<1.95) & (cvsig>0.5)))
+set3 <- intersect(set1,set2)
+highervars <- set3
+data.mat <- polIIgene.nm.sigint[highervars,c(1,2,4,5)]
 
 m <- data.mat
 
@@ -83,6 +90,43 @@ rob1 <- c(y1.membrob,y2.membrob,y3.membrob,y4.membrob,y5.membrob)
 rob <- rob1[highervars] ## to reorder 
 clusters.p2sigint <- cbind(clusters,rob)
 colnames(clusters.p2sigint) <- c("Cluster","Robustness")
+
+
+##
+##
+## clrob(cmr.pam$e1_pam_k4)
+##        rob
+##1 1.0000000
+##2 0.9932455
+##3 0.9969556
+##4 0.9528354
+##
+## k=4
+##
+clusters <- cmr.pam$e1_pam_k4@rm
+
+## member robustness
+y1 <- memrob(cmr.pam$e1_pam_k4)$cluster1
+y1.ps <- rownames(y1@mrl) ## labels
+y1.membrob <- y1@mrl[["mem_rob"]] ## values
+names(y1.membrob) <- y1.ps
+y2 <- memrob(cmr.pam$e1_pam_k4)$cluster2
+y2.ps <- rownames(y2@mrl) ## labels
+y2.membrob <- y2@mrl[["mem_rob"]] ## values
+names(y2.membrob) <- y2.ps 
+y3 <- memrob(cmr.pam$e1_pam_k4)$cluster3
+y3.ps <- rownames(y3@mrl) ## labels
+y3.membrob <- y3@mrl[["mem_rob"]] ## values
+names(y3.membrob) <- y3.ps 
+y4 <- memrob(cmr.pam$e1_pam_k4)$cluster4
+y4.ps <- rownames(y4@mrl) ## labels
+y4.membrob <- y4@mrl[["mem_rob"]] ## values
+names(y4.membrob) <- y4.ps
+rob1 <- c(y1.membrob,y2.membrob,y3.membrob,y4.membrob)
+rob <- rob1[highervars] ## to reorder 
+clusters.p2sigint <- cbind(clusters,rob)
+colnames(clusters.p2sigint) <- c("Cluster","Robustness")
+
 
 save(clusters.p2sigint,file="clusters.p2sigint.RData")
 
