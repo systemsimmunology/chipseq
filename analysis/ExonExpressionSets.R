c92284ce0d37d9f700348099b5792bb8f2e50a69
Vesteinn Thorsson <thorsson@systemsbiology.org>
2013-03-04 17:51:36 -0800

    ExonExpressionSets.R updated

--- analysis/ExonExpressionSets.R
+++ analysis/ExonExpressionSets.R
@@ -1,59 +1,81 @@
-load("~/allarrays/data/20100407.curated.exon/CSSs.tc.RData")
-load("~/allarrays/data/20100407.curated.exon/dm.RData")
-dm.lps.exon <- dm
-CSSs.tc.exon <- CSSs.tc
 ##
-## Differentially Expresssed
+##  Load data
+## 
+data.dir <- paste(Sys.getenv("AA"),"data",sep="/")
+direxon.cur <- paste(data.dir, "20121001.curated.exon", sep = "/")
+load(paste(direxon.cur, "lps.lambdas.RData", sep = "/"))
+load(paste(direxon.cur, "lps.mus.RData", sep = "/"))
+load(paste(direxon.cur, "lps.ratios.RData", sep = "/"))
+ncbiID <- as.character(unlist(sapply(rownames(lps.mus),strsplit,split="_at")))
+names(ncbiID) <- rownames(lps.mus)
+sigSlice <- function( lambdaCutoff , ratioMatrix, lambdaMatrix){
+  whichOnes <- unique(row.names(which(lambdaMatrix>lambdaCutoff,arr.ind=TRUE)))
+  ratioMatrix[whichOnes,]  
+}
+on.exon.array.eid <- as.character(ncbiID[rownames(lps.mus)])
+
 ##
+## Determine differentially expressed and constitutive genes
+## 
 
-## binarization, with max time six hours
-abs.cutoff <- 200 ## Same as used for timecourse binarization
-rat.cutoff <- 2.0 ## Same as used for timecourse binarization
+mu.cutoff.3prime <- 300
+mu.high.cutoff.3prime <- 300
+per90.ratio <-  2.429
+mu.cutoff.exon <- per90.ratio * mu.cutoff.3prime
+mu.high.cutoff.exon <- per90.ratio * mu.high.cutoff.3prime
+lambda.cutoff <- 26.44526  ## 0.05% cutoff September 2012
+imax <- 5  ## imax=5 <-> 6 hrs
 
-## nt=5 corresponds to 6 hrs
-nt <- 5
-maxabs <- apply(dm.lps.exon[,1:nt],1,max)
-abs.logvec <- ( maxabs > abs.cutoff )
-tcs <- dm.lps.exon
-ratmat <- (tcs/tcs[,1])[,2:nt]
-maxrats <- apply(ratmat,1,max)
-minrats <- apply(ratmat,1,min)
-rat.logvec <- ( maxrats > rat.cutoff ) | ( minrats < (1/rat.cutoff) )
-diffexp.exon.eid <- names(which(abs.logvec & rat.logvec))
-
-mu.high.cutoff <- 300
-high.expressors <- names(which(apply(dm.lps.exon[,1:nt]>mu.high.cutoff,1,sum)==nt))
-constitutive.exon.eid <- setdiff(high.expressors,diffexp.exon.eid)
+lps.6hr.exon.ps <- rownames(sigSlice(lambda.cutoff, 
+                                     lps.ratios[, 1:(imax - 
+                                                                           1)], lps.lambdas[, 1:(imax - 1)]))
+low.expressors <- names(which(apply(lps.mus[lps.6hr.exon.ps, 1:imax] < 
+                                      mu.cutoff.exon, 1, sum) == imax))
+lps.6hr.exon.ps <- setdiff(lps.6hr.exon.ps, low.expressors)
+lps.6hr.exon.eid <- as.character(ncbiID[lps.6hr.exon.ps])
+diffexp.exon.eid <- lps.6hr.exon.eid
+  
+imax <- 5  ## imax=5 <-> 6 hrs
+high.expressors <- names(which(apply(lps.mus[, 1:imax] > mu.high.cutoff.exon, 
+                                     1, sum) == imax))
+constitutive.exon.ps <- setdiff(high.expressors, lps.6hr.exon.ps)
+constitutive.exon.eid <- as.character(ncbiID[constitutive.exon.ps])
 
 ## signal integral to 4hrs
 imax <- 4
-m <- dm.lps.exon[,1:imax]
+m <- lps.mus[,1:imax]
+rownames(m) <- on.exon.array.eid
 mm <- (m[,2:imax]+m[,1:(imax-1)])/2
 tvec <- c(0,60,120,240)
 tchange <- tvec[2:length(tvec)]-tvec[1:(length(tvec)-1)]
 mmm <- t(t(mm) * tchange)
 intvec <- apply(mmm,1,sum)
 lps.mic <- intvec/(tvec[length(tvec)]-tvec[1]) - m[,1]
-si4 <- lps.mic[diffexp.exon.eid] ## signal integral
+si4 <- lps.mic[lps.6hr.exon.eid] ## signal integral
 ivec <- (sign(si4)+3)/2
 b <- c("Repressed","Induced")
 bb <- b[ivec] ## vector of Repressed, Induced for diffexp.exon.eid
-names(bb) <- diffexp.exon.eid
+names(bb) <- lps.6hr.exon.eid
 
-setmat <- matrix(0,nrow=nrow(dm.lps.exon),ncol=6)
-rownames(setmat) <- rownames(dm.lps.exon)
+##
+## Set up matrix in terms of gene IDs
+## 
+
+setmat <- matrix(0,nrow=length(on.exon.array.eid),ncol=6)
+rownames(setmat) <- on.exon.array.eid
 colnames(setmat) <- c("On Exon Array","Constitutive Expression - Exon","Differential Expression - Exon","Quantitative Change - Exon","Qualitative Change - Exon","Cluster - Exon")
 setmat[,1]=1
-setmat[,2]=(rownames(dm.lps.exon) %in%  constitutive.exon.eid )*1
-setmat[,3]=(rownames(dm.lps.exon) %in%  diffexp.exon.eid )*1
+setmat[,2]=(on.exon.array.eid %in%  constitutive.exon.eid )*1
+setmat[,3]=(on.exon.array.eid %in%  diffexp.exon.eid )*1
 setmat[,4]=NA
 setmat[diffexp.exon.eid,4]=round(lps.mic[diffexp.exon.eid],2)
 setmat[,5]="Below Threshold"
 setmat[diffexp.exon.eid,5]=bb[diffexp.exon.eid]
 setmat[constitutive.exon.eid,5]="Constitutive"
 
-data.mat <- log10(ratmat[diffexp.exon.eid,])
-data.mat.w0 <- cbind(rep(0,length(diffexp.exon.eid)),data.mat)
+## we will use correlations to ratios below
+data.mat <- lps.ratios[lps.6hr.exon.ps,1:imax]
+data.mat.w0 <- cbind(rep(0,length(lps.6hr.exon.ps)),data.mat)
 colnames(data.mat.w0) <- c("min0",colnames(data.mat))
 m <- data.mat.w0
 
@@ -61,17 +83,13 @@ m <- data.mat.w0
 ## Assign clusters by comparing with previously computer from 3prime
 ##
 ## Pre-amble to get 3' matrix used for clustering
-load("~/allarrays/data/20100426.curated.3prime/all.lambdas.objects.RData")
-load("~/allarrays/data/20100426.curated.3prime/all.ratios.objects.RData")
-load("~/allarrays/data/20100426.curated.3prime/all.mus.objects.RData")
-lambda.cutoff <- 66.31579 ## 0.01% cutoff - leads to 3069 genes for full time-course, at mu.cutoff 100
-##lambda.cutoff <- 26.61275 ## 0.05% cutoff - leads to 4913 genes for full time-course
+load("~/allarrays/data/20120926.curated.3prime/all.lambdas.objects.RData")
+load("~/allarrays/data/20120926.curated.3prime/all.ratios.objects.RData")
+load("~/allarrays/data/20120926.curated.3prime/all.mus.objects.RData")
+lambda.cutoff <- 67.22123 ## 0.01% cutoff - September 2012
+##lambda.cutoff <- 26.44526 ## 0.05% cutoff - September 2012 
 mu.cutoff <- 300
 imax <- 8 ## imax=8 <-> 6 hrs 
-sigSlice <- function( lambdaCutoff , ratioMatrix, lambdaMatrix){
-  whichOnes <- unique(row.names(which(lambdaMatrix>lambdaCutoff,arr.ind=TRUE)))
-  ratioMatrix[whichOnes,]  
-}
 lps.6hr.ps <- rownames(sigSlice(lambda.cutoff,lps.ratios[,1:(imax-1)],lps.lambdas[,1:(imax-1)]))
 low.expressors <- names(which(apply(lps.mus[lps.6hr.ps,1:imax]<mu.cutoff,1,sum)==imax))
 lps.6hr.ps <- setdiff(lps.6hr.ps,low.expressors)
@@ -80,8 +98,10 @@ data.mat.w0 <- cbind(rep(0,length(lps.6hr.ps)),data.mat)
 colnames(data.mat.w0) <- c("min0",colnames(data.mat))
 m3 <- data.mat.w0
 m3r <- m3[,c("min0","min60","min120","hr4","hr6")]
+## we need this for the correlation calculation below
+
 ## Pre-amble for 3' membership and robustness
-load("~/chipseq/results/20120323/clusters.3prime.RData")
+load("~/chipseq/results/20130227/clusters.3prime.RData")
 ## gives clusters.3prime
 clustmat <- clusters.3prime
 clustered.pss <- rownames(clustmat)
